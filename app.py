722d47159e619f9c7101e04e3674d9bbd76fe74f
Jonasperegrino <haag.jonas@gmail.com>
2022-07-01 07:38:05 +0000

    new regio

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.set_page_config(
 
 # models
 topic = load_model("keras/topic", custom_objects=ak.CUSTOM_OBJECTS)
-regional = load_model("keras/regional", custom_objects=ak.CUSTOM_OBJECTS)
+regional = load_model("keras/regio", custom_objects=ak.CUSTOM_OBJECTS)
 # conversion = load_model("keras/topic", custom_objects=ak.CUSTOM_OBJECTS)
 reach = load_model("keras/reach", custom_objects=ak.CUSTOM_OBJECTS)
 engagement = load_model("keras/engagement", custom_objects=ak.CUSTOM_OBJECTS)
@@ -90,7 +90,8 @@ body = st.text_area(
 )
 
 
-def scorer(badge, threshold, value):
+def scorer(badge, threshold, prediction):
+    value = prediction[0][0].astype(float)
     if value > threshold:
         return badge
     elif badge == "Regional":
@@ -99,25 +100,22 @@ def scorer(badge, threshold, value):
 
 # magic
 if st.button("Los"):
-    text = np.array([body])
-    topic_prob = topic.predict(tf.expand_dims(text, -1))
+    text = tf.expand_dims(np.array([body]), -1)
+
+    topic_prob = topic.predict(text)
     topic_badge = topics[np.argmax(topic_prob)]
-    
-    regional_badge = scorer(
-        "Regional", 0.7, regional.predict(tf.expand_dims(text, -1))[0][0].astype(float)
-    )
-    reach_badge = scorer(
-        "Reach", 0.3, reach.predict(tf.expand_dims(text, -1))[0][0].astype(float)
-    )
+
+    regional_badge = scorer("Regional", 0.7, regional.predict(text))
+    reach_badge = scorer("Reach", 0.3, reach.predict(text))
     engagement_badge = scorer(
         "Engagement",
         0.3,
-        engagement.predict(tf.expand_dims(text, -1))[0][0].astype(float),
+        engagement.predict(text),
     )
     antichurn_badge = scorer(
         "Anti-Churn",
         0.3,
-        antichurn.predict(tf.expand_dims(text, -1))[0][0].astype(float),
+        antichurn.predict(text),
     )
     st.header(f"Thema: {topic_badge.title()}")
     st.header(f"{regional_badge or ''}")
